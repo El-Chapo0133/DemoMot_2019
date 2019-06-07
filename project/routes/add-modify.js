1283e093875e7a95476a64b32235fb31fc8893e8
Loris <levequelo@etml.educanet2.ch>
2019-06-07 06:33:24 +0200

    initial routes made - v0.1

---
+++ project/routes/add-modify.js
@@ -0,0 +1,48 @@
+let api = require('../api/api');
+let ejs = require('ejs');
+
+module.exports = {
+    modify: (request, response, idCard) => {
+        const SQL = "SELECT idCard, carTitle, carContent, carDesc, carMetrique FROM Card WHERE idCard = '" + String(idCard) + "'";
+        var database = new api;
+
+        var connector = database.createConnector();
+
+        var dataFromDB = database.execute(connector, SQL);
+
+        obj = {
+            "card": dataFromDB
+        }
+
+        var toDisplay = ejs.renderFile('../views/add-modify.ejs', obj, (err, str) => {
+            if (err) {
+                console.log("failed load add-modify.ejs");
+                throw err;
+            } else {
+                console.log("add-modify loaded");
+                return str;
+            }
+        });
+
+        response.WriteHead(200, {"Content-Type": "text/html"});
+        response.write(toDisplay);
+        response.end();
+    },
+    add: (request, response) => {
+        
+
+        var toDisplay = ejs.renderFile('../views/add-modify.ejs', null, (err, str) => {
+            if (err) {
+                console.log("failed load add-modify.ejs");
+                throw err;
+            } else {
+                console.log("add-modify loaded");
+                return str;
+            }
+        });
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(toDisplay);
+        response.end();
+    }
+}
--- project/routes/index.js
+++ project/routes/index.js
@@ -1,11 +1,31 @@
 let api = require("../api/api");
+let ejs = require('ejs');
 
 module.exports = {
     index: (request, response) => {
+        const SQL = "SELECT idCard, carTitle, carContent, carDesc, carMetrique FROM Card";
         var database = new api;
 
         var connector = database.createConnector();
         
-        var data = database.executeSql(connector, "SELECT * FROM User");
+        var dataFromDB = database.executeSql(connector, SQL);
+
+        obj = {
+            "cards": dataFromDB
+        };
+
+        var toDisplay = ejs.renderFile("../views/main.ejs", ebj, (err, str) => {
+            if (err) {
+                console.log("failed load index | err:" + err);
+                throw err;
+            } else {
+                console.log("index loaded!");
+                return str;
+            }
+        });
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(toDisplay);
+        response.end();
     }
 }
